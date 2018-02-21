fa5e5572cd7479d160833c3df000778b2b1a67cc
johntalton <johntalton@gmail.com>
2018-02-21 04:33:49 +0000

    helper function

--- src/rasbus.js
+++ src/rasbus.js
@@ -1,8 +1,25 @@
+"use strict";
 
-module.exports = {
-  pispi: require('./impl-pi-spi'),
-  spi: require('./impl-spi'),
+class Rasbus {
+  static byname(name) {
+    switch(name.toLowerCase()) {
+      case 'pispi': return Rasbus.pispi; break;
+      case 'spi': return Rasbus.spi; break;
 
-  i2c: require('./impl-i2c'),
-  i2cbus: require('./impl-i2c-bus')
-};
+      case 'i2cbus': return Rasbus.i2cbus; break;
+      case 'i2c': return Rasbus.i2c; break;
+
+      default:
+        throw Error('unknown bus name', name);
+    }
+  }
+}
+
+Rasbus.pispi = require('./impl-pi-spi'),
+Rasbus.spi = require('./impl-spi'),
+
+Rasbus.i2c = require('./impl-i2c'),
+Rasbus.i2cbus = require('./impl-i2c-bus')
+
+
+module.exports = Rasbus;
