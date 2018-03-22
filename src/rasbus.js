0f2129abe84b132364140075f8d8cc0438778216
johntalton <johntalton@gmail.com>
2018-03-22 23:59:01 +0000

    clean up bus selection

--- src/rasbus.js
+++ src/rasbus.js
@@ -1,26 +1,37 @@
 "use strict";
 
+/**
+ *
+ **/
 class Rasbus {
   static byname(name) {
-    switch(name.toLowerCase()) {
-      case 'pispi': return Rasbus.pispi; break;
-      case 'spi': return Rasbus.spi; break;
-
-      case 'raspii2c': return Rasebus.raspii2c; break;
-      case 'i2cbus': return Rasbus.i2cbus; break;
-      case 'i2c': return Rasbus.i2c; break;
-
-      default:
-        throw Error('unknown bus name: ' + name);
+    const bus = Rasbus.busMap.find(item => item.name === name.toLowerCase());
+    if(bus === undefined) { throw Error('unknown bus name: ' + name); }
+    if(bus._impl === undefined) {
+      bus._impl = require(bus.impl);
     }
+    return bus._impl;
+  }
+
+  static names(type) {
+    const filtered = (type === undefined) ?
+      Rasbus.busMap :
+      Rasbus.busMap.filter(item => item.type === type.toLowerCase());
+    return filtered.map(item => item.name);
   }
 }
 
-Rasbus.pispi = require('./impl-pi-spi');
-Rasbus.spi = require('./impl-spi');
+Rasbus.busMap = [
+  // I2C
+  // { name: 'raspii2c', type: 'i2c', impl: './impl-i2c-raspii2c' },
+  { name: 'i2cbus', type: 'i2c', impl: './impl-i2c-bus' },
+  { name: 'i2c',    type: 'i2c', impl: './impl-i2c' },
+  // SPI
+  { name: 'pispi',  type: 'spi', impl: './impl-pi-spi' },
+  { name: 'spi',    type: 'spi', impl: './impl-spi' },
+];
 
-Rasbus.i2c = require('./impl-i2c');
-Rasbus.i2cbus = require('./impl-i2c-bus');
-//Rasbus.raspii2c = require('./impl-i2c-raspii2c');
+
+
 
 module.exports = Rasbus;
